4ab0faf1f6c3f00f7a217351f6430a9dc70f3650
Trevor Lewis <tdlewis7@gmail.com>
2019-04-23 10:47:31 -0600

    Add transactions to wallets

--- src/main/java/blockchain/Block.java
+++ src/main/java/blockchain/Block.java
@@ -1,20 +1,22 @@
 package main.java.blockchain;
 
 import main.java.util.StringUtil;
+import main.java.wallet.Transaction;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; // Our data will be a simple message.
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<>(); // Our data will be a simple message.
     private long timeStamp; // As number of milliseconds since 1/1/1970.
     private int nonce;
 
     // Block Constructor
-    public Block(String data, String previousHash) {
-        this.data = data;
+    public Block(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
 
@@ -27,11 +29,13 @@ public class Block {
                 previousHash +
                         timeStamp +
                         nonce +
-                        data
+                        merkleRoot
         );
     }
 
     public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+
         // Create a string with difficulty * "0"
         String target = StringUtil.getDificultyString(difficulty);
 
@@ -40,6 +44,23 @@ public class Block {
             hash = calculateHash();
         }
         System.out.println("Block Mined!!!!: " + hash);
+    }
+
+    // Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        // Process transaction and check if valid, unless block is genesis block then ignore.
+        if (transaction == null) {
+            return false;
+        }
+        if (!previousHash.equals("0")) {
+            if (!transaction.processTransaction()) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction successfully added to Block!");
+        return true;
     }
 
 }
--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -1,9 +1,11 @@
 package main.java.util;
 
 import com.google.gson.GsonBuilder;
+import main.java.wallet.Transaction;
 
 import java.io.UnsupportedEncodingException;
 import java.security.*;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class StringUtil {
@@ -70,6 +72,28 @@ public class StringUtil {
 
     public static String getStringFromKey(Key key) {
         return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    // Tacks in array transactions and returns a merkle root.
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+        ArrayList<String> previousTreeLayer = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionId);
+        }
+
+        ArrayList<String> treeLayer = previousTreeLayer;
+        while (count > 1) {
+            treeLayer = new ArrayList<>();
+            for (int i = 1; i < previousTreeLayer.size(); i++) {
+                treeLayer.add(applySha256(previousTreeLayer.get(i - 1) + previousTreeLayer.get(i)));
+            }
+
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+        return (treeLayer.size() == 1) ? treeLayer.get(0) : "";
     }
 
 }
